a19af0abeb13f4cf9b19da3f49258d93c955c4a0
squidrye <mihirs167@gmail.com>
2022-10-22 01:39:01 +0530

    added copy with in model class

--- lib/models/api_model.dart
+++ lib/models/api_model.dart
@@ -10,12 +10,38 @@ class Movie {
 
   Movie(
       {required this.director,
-        required this.actor,
-        required this.plot,
-        required this.title,
-        required this.imgUrl,
-        required this.genre,
-        required this.rating,
-        required this.runtime});
+      required this.actor,
+      required this.plot,
+      required this.title,
+      required this.imgUrl,
+      required this.genre,
+      required this.rating,
+      required this.runtime});
 
+  Movie copyWith({
+    String? title,
+    String? imgUrl,
+    String? genre,
+    String? rating,
+    String? director,
+    String? actor,
+    String? plot,
+    String? runtime,
+  }) {
+    return Movie(
+      title: title ?? this.title,
+      imgUrl: imgUrl ?? this.imgUrl,
+      genre: genre ?? this.genre,
+      rating: rating ?? this.rating,
+      director: director ?? this.director,
+      actor: actor ?? this.actor,
+      plot: plot ?? this.plot,
+      runtime: runtime ?? this.runtime,
+    );
+  }
+
+  @override
+  String toString() {
+    return "{title:$title + genre:$genre + imgUrl:$imgUrl + rating:$rating + director:$director + actor:$actor+ plot:$plot + runtTime:$runtime}";
+  }
 }
