ca28ea2f2d6a7f822c0ecdad0695be109f08e36f
Devank Sachdeva <96934308+Devank-Sachdeva@users.noreply.github.com>
2022-10-25 19:55:45 +0530

    Added Image Picker For profilePage.dart (#12)

--- lib/views/profilePage.dart
+++ lib/views/profilePage.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class profilePage extends StatefulWidget {
   // const profilePage({super.key});
@@ -8,7 +13,44 @@ class profilePage extends StatefulWidget {
 }
 
 class _profilePageState extends State<profilePage> {
-  bool isObsecurePassword = true;
+  @override
+  void initState() {
+    fetchPickedImage();
+    super.initState();
+  }
+
+  bool isObscurePassword = true;
+  String fetchPickedImagePath = '';
+
+  Future<void> fetchPickedImage() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    final imagePath = prefs.getString('profile_image');
+    if (imagePath != null) {
+      setState(() {
+        fetchPickedImagePath = imagePath;
+      });
+    }
+  }
+
+  Future<void> pickTheImage() async {
+    ImagePicker picker = ImagePicker();
+    final imageSelected = await picker.pickImage(source: ImageSource.gallery);
+    if (imageSelected == null) return;
+
+    File tmpFile = File(imageSelected.path);
+    final Directory appDirectory = await getApplicationDocumentsDirectory();
+    final actualPath = appDirectory.path;
+
+    final String fileName = basename(imageSelected.path);
+    tmpFile = await tmpFile.copy('$actualPath/$fileName');
+
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setString('profile_image', tmpFile.path);
+    setState(() {
+      fetchPickedImage();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,27 +76,34 @@ class _profilePageState extends State<profilePage> {
                     shape: BoxShape.circle,
                     image: DecorationImage(
                       fit: BoxFit.cover,
-                      image: AssetImage("assets/img.jpg"),
+                      image: (fetchPickedImagePath == '')
+                          ? AssetImage("assets/img.jpg")
+                          : FileImage(File(fetchPickedImagePath)) as ImageProvider,
                     ),
                   ),
                 ),
                 Positioned(
                   bottom: 0,
                   right: 0,
-                  child: Container(
-                    height: 40,
-                    width: 40,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        width: 4,
+                  child: GestureDetector(
+                    onTap: () async {
+                      pickTheImage();
+                    },
+                    child: Container(
+                      height: 40,
+                      width: 40,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          width: 4,
+                          color: Colors.white,
+                        ),
+                        color: Colors.blue,
+                      ),
+                      child: Icon(
+                        Icons.edit,
                         color: Colors.white,
                       ),
-                      color: Colors.blue,
-                    ),
-                    child: Icon(
-                      Icons.edit,
-                      color: Colors.white,
                     ),
                   ),
                 ),
@@ -74,6 +123,13 @@ class _profilePageState extends State<profilePage> {
             children: [
               OutlinedButton(
                 onPressed: () {},
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.blue),
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
                 child: Text(
                   "CANCEL",
                   style: TextStyle(
@@ -82,28 +138,22 @@ class _profilePageState extends State<profilePage> {
                     color: Colors.white,
                   ),
                 ),
-                style: OutlinedButton.styleFrom(
-                  side: BorderSide(color: Colors.blue),
-                  padding: EdgeInsets.symmetric(horizontal: 50),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                ),
               ),
               ElevatedButton(
                 onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: EdgeInsets.symmetric(horizontal: 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
                 child: Text(
                   "SAVE",
                   style: TextStyle(
                     fontSize: 15,
                     letterSpacing: 2,
                     color: Colors.white,
-                  ),
-                ),
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.blue,
-                  padding: EdgeInsets.symmetric(horizontal: 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
                   ),
                 ),
               ),
@@ -114,8 +164,7 @@ class _profilePageState extends State<profilePage> {
     );
   }
 
-  Widget buildTextField(
-      String labelText, String placeHolder, bool isPasswordTextField) {
+  Widget buildTextField(String labelText, String placeHolder, bool isPasswordTextField) {
     return Padding(
       padding: EdgeInsets.only(bottom: 30),
       child: TextField(
